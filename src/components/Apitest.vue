937860113ef54c770c50bb38cf4b883cbd502f32
Goran Blazin <goran.blazin@gmail.com>
2017-07-16 22:48:13 +0200

    Finished api test

--- src/components/Apitest.vue
+++ src/components/Apitest.vue
@@ -1,15 +1,49 @@
 <template>
 	<div class="apitest">
-		<h1>Api test</h1>
+		<h1>Api test that queries wikipedia</h1>
 		<router-link to="/">Home</router-link>
+
+		<h2>Wikipedia top 10 search results:</h2>
+		<input v-model="apiSearch" />
+		<span v-if="loading">Loading wiki search...</span>
+		<ul v-if="wikiSearchResults.length">
+			<li v-for="(search, index) in wikiSearchResults">
+				{{index+1}}. {{search}}
+			</li>
+		</ul>
 	</div>
 </template>
 
 <script>
+	import axios from 'axios';
+	import _ from 'lodash';
 	export default {
-		name: 'apitest',
+		name: "apitest",
 		data: function() {
-			return {};
+			return {
+				apiSearch: "",
+				loading: false,
+				wikiSearchResults: []
+			};
+		},
+		watch: {
+			apiSearch: _.debounce(function() {
+				const self = this;
+				if (self.apiSearch) {
+					self.loading = true;
+					self.wikiSearchResults = [];
+					axios.get(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${self.apiSearch}&format=json&origin=*`)
+						.then(function({ data }) {
+							self.loading = false;
+							self.wikiSearchResults = data.query.search.map(function(item) {
+								return item.title;
+							});
+						}).catch(function (err) {
+							self.wikiHtml = err.message;
+						});
+				}
+
+			}, 500)
 		}
 	}
 </script>
